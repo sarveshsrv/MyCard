e7f67c6bd0840ba367ae77bd3a3cd79b1f7f158e
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-07-04 17:27:09 +0530

    Scaffold updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+          body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/img.png'),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
 }
