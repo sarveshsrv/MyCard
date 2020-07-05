c10881b5227f9c617b5a5d793357c8b3e4ba5357
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-07-05 14:29:10 +0530

    UI updated

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,125 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          backgroundColor: Colors.teal,
           body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/img.png'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/img.png'),
+                ),
+                Text(
+                  "Sarvesh Tiwari",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'Galada',
+                    color: Colors.white,
+                    // fontWeight:FontWeight.bold,
+                    letterSpacing: 2.0,
+                    //fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontFamily: 'LobsterTwo',
+                    color: Colors.teal.shade100,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold,
+                    //decoration: TextDecoration.underline,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      size: 35,
+                    ),
+                    title: Text(
+                      " +91 8505926675",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        wordSpacing: 3,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ),
+                /*Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 35),
+                  padding: EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                        size: 35,
+                      ),
+                      SizedBox(width: 20),
+                      Text(
+                        " +91 8505926675",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          wordSpacing: 5,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),*/
+
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 6, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                      size: 35,
+                    ),
+                    title: Text(
+                      " sarveshFlutterDev@gmail.com",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        wordSpacing: 2,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                /* Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 6, horizontal: 35),
+                  padding: EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                        size: 35,
+                      ),
+                      SizedBox(width: 20),
+                      Text(
+                        " sarveshFlutterDev@gmail.com",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          wordSpacing: 2,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),*/
+              ],
             ),
-          ],
-        ),
-      )),
+          )),
     );
   }
 }
